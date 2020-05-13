c039682b0d071e828895521d7e784d414022c3c9
Christine Bussinger <christinebussingerdev@gmail.com>
2020-05-13 01:36:32 -0400

    finished project

---
+++ api/server.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const server = express();
+server.get("/", (req, res) => {
+  res.json({ message: "welcome to api" });
+});
+module.exports = server;
---
+++ users/users-router.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const Users = require("./users-model.js");
+router.get("/", async (req, res) => {
+  console.log("token", req.decodedToken);
+  try {
+    res.status(201).json(await Users.find());
+  } catch (error) {
+    res.status(400).json({ errorMessage: "Failed to retrieve users" });
+  }
+});
+module.exports = router;
